fd07d47669f6b15150edd0b8b34d6c1587e070b0
argonauta <git@marcinnaskret.pl>
2018-02-14 22:56:56 +0100

    Update settings.py

--- coincheck/settings.py
+++ coincheck/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9@v&jrwd($)^8ntc17p7_g0)oh^*&zjf^(bfu6l%9&1f0kee%j'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
